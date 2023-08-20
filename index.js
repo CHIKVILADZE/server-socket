507fccbe7e2afe7811a8ef50f99d6bc4bae966e3
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-08-20 14:15:23 +0400

    remove timeout

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const dbConnection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectTimeout: 60000,
+  // connectTimeout: 60000,
 });
 
 dbConnection.connect((err) => {
